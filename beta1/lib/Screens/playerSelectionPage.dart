3273da4f11f90cc9f4d136c87e9e8e04fe283f64
Rayyan Zaid <rayyanzaid0401@gmail.com>
2023-01-12 18:21:00 -0800

    converted decimal to percentage

--- beta1/lib/Screens/playerSelectionPage.dart
+++ beta1/lib/Screens/playerSelectionPage.dart
@@ -192,7 +192,6 @@ class PlayerSelectionPageState extends State<PlayerSelectionPage> {
             Map<String, Map<String, dynamic>> sortedMap = Map.fromIterable(keys,
                 key: (k) => k, value: (k) => playerToStats[k]);
             debugPrint(sortedMap.toString());
-
             // Return 2 lists (one for each team)
 
             // Azure
